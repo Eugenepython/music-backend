01dbbf1dfb83bed6753ef09c3f477f96a99d58f7
Eugenepython <eugene_doris@hotmail.com>
2024-02-21 16:52:28 +0000

    commiting

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import nodemailer from 'nodemailer';
 import dotenv from 'dotenv';
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 console.log("hello")
 app.use(bodyParser.json());
 dotenv.config();
